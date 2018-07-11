6c92471a4a4ca717db991ffdaaaa51e26b9d20d6
Philipp Rentzsch <philipp.rentzsch@bihealth.de>
2018-07-11 20:32:21 +0200

    fixed some problems with empty files and filenames - parse filenames based on ".vc" substring instead of generic "." - prevent pipefail from file stream being interuÃpted by head in large files - prevent throwing error on sorting empty files - added error mode for undefined variables

--- CADD.sh
+++ CADD.sh
@@ -38,13 +38,12 @@ INFILE=$1
 
 echo "CADD-v1.4 (c) University of Washington, Hudson-Alpha Institute for Biotechnology and Berlin Institute of Health 2013-2018. All rights reserved."
 
-set -e
-set -o pipefail
+set -ueo pipefail
 
 ### Configuring all the paths
 
 FILENAME=$(basename $INFILE)
-NAME=${FILENAME/\.*/}
+NAME=${FILENAME/\.vc*/}
 FILEDIR=$(dirname $INFILE)
 FILEFORMAT=${FILENAME/$NAME\./}
 
@@ -165,11 +164,11 @@ then
 fi
 
 # Join pre and novel scored variants
-(
+{
     echo "##CADD $GENOMEBUILD-v1.4 (c) University of Washington, Hudson-Alpha Institute for Biotechnology and Berlin Institute of Health 2013-2018. All rights reserved.";
-    zcat $TMP_NOV | head -n 1;
-    zcat $TMP_PRE $TMP_NOV | grep -v "^#" | sort -k1,1 -k2,2n -k3,3 -k4,4;
-) | bgzip -c > $OUTFILE;
+    head -n 1 < <(zcat $TMP_NOV);
+    zcat $TMP_PRE $TMP_NOV | grep -v "^#" | sort -k1,1 -k2,2n -k3,3 -k4,4 || true;
+} | bgzip -c > $OUTFILE;
 rm $TMP_NOV
 rm $TMP_PRE
 
