716345b3ad06d7f7edca53decc5dc29f80939c0a
Philipp Rentzsch <philipp.rentzsch@bihealth.de>
2020-01-02 16:26:28 +0100

    Added position check to prevent InDels being called by mistake. Fixes #14 + some small changes in file handling of the script

--- src/scripts/extract_scored.py
+++ src/scripts/extract_scored.py
@@ -7,16 +7,22 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-p","--path", dest="path", help="Path to scored variants.")
-parser.add_option("--found_out", dest="found_out", help="Write found variants to file (default 'output.tsv')",default=None)
+parser.add_option("-i","--input", dest="input", help="Read variants from vcf file (default stdin)",default=None)
+parser.add_option("--found_out", dest="found_out", help="Write found variants to file (default: stdout)",default=None)
 parser.add_option("--header", dest="header", help="Write full header to output (default none)",default=False, action="store_true")
 (options, args) = parser.parse_args()
+
+if options.input:
+    stdin = open(options.input,'r')
+else:
+    stdin = sys.stdin
 
 if options.found_out:
     found_out = open(options.found_out,'w')
 else:
     found_out = sys.stdout
 
-fref,falt = 2,3
+fpos,fref,falt = 1,2,3
 if os.path.exists(options.path) and os.path.exists(options.path+".tbi"):
   filename = options.path
   sys.stderr.write("Opening %s...\n"%(filename))
@@ -33,7 +39,7 @@ if os.path.exists(options.path) and os.path.exists(options.path+".tbi"):
 else:
   raise IOError("No valid file with pre-scored variants.\n")
 
-for line in sys.stdin:
+for line in stdin:
   line = line.rstrip('\n\r')
   if line.startswith('#'):
     sys.stdout.write(line + '\n')
@@ -47,9 +53,10 @@ for line in sys.stdin:
     lref,allele = fields[-2],fields[-1]
     for regionHit in regionTabix.fetch(chrom, pos-1, pos):
       vfields = regionHit.rstrip().split('\t')
-      if (vfields[fref] == lref) and (vfields[falt] == allele):
+      if (vfields[fref] == lref) and (vfields[falt] == allele) and (vfields[fpos] == pos)):
         found_out.write(regionHit+"\n")
         found = True
+        break
 
     if not found:
       sys.stdout.write(line + '\n')
@@ -57,6 +64,9 @@ for line in sys.stdin:
   except ValueError:
     sys.stderr.write('Encountered uncovered chromosome\n')
     sys.stdout.write(line + '\n')
+
+if options.input:
+    stdin.close()
 
 if options.found_out:
     found_out.close()
